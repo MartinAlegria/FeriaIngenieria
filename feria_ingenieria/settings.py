8aa82366b17ff80a1f4086b4a232cbedc3457163
MartinAlegria <A01022216@itesm.mx>
2019-11-19 22:25:47 -0600

    Orasi este es el wueno

--- feria_ingenieria/settings.py
+++ feria_ingenieria/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '&^*1n31ap7!x_cuc1&^$%0c@z3h@r)u6lzq_qg+(1bf#^xyko+'
 DEBUG = True
 #DEBUG = False
 
-ALLOWED_HOSTS = ['expodic19-csf.herokuapp.com', '127.0.0.1'. 'www.expodic-csf.com', 'expodic-csf.com']
+ALLOWED_HOSTS = ['expodic19-csf.herokuapp.com', '127.0.0.1', 'www.expodic-csf.com', 'expodic-csf.com']
 
 
 # Application definition
